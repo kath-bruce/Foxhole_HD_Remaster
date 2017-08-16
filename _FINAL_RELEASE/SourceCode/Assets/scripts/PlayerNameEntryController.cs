0d50af8eee4e76bdad54937ec4d5ea9bad1390ee
Kathleen Bruce <kath-bruce@hotmail.com>
2017-08-16 23:18:22 +0100

    Minor bug fix on player name entry

--- _FINAL_RELEASE/SourceCode/Assets/scripts/PlayerNameEntryController.cs
+++ _FINAL_RELEASE/SourceCode/Assets/scripts/PlayerNameEntryController.cs
@@ -8,20 +8,53 @@ public class PlayerNameEntryController : MonoBehaviour
     public AudioClip bleep;
     public AudioClip bloop;
 
+    public TMPro.TMP_InputField playerName;
+
+    public GameObject txtPleaseEnterName;
+
     public void btnBackClick()
     {
         GlobalController.INSTANCE.PlaySound(bloop);
+        txtPleaseEnterName.SetActive(false);
         gameObject.SetActive(false);
     }
 
     public void txtPlayerNameEnter(string input) //the input field triggers this method whenever unfocussed
     {
+        if (input == "" || input == null)
+        {
+            txtPleaseEnterName.SetActive(true);
+            return;
+        }
+        else
+        {
+            txtPleaseEnterName.SetActive(false);
+        }
+
         GlobalController.INSTANCE.CurrentPlayerName = input;
 
-        if (Input.GetKey(KeyCode.Return)) //this limits the game being loaded to when the player hits carriage return
+        //this limits the game being loaded to when the player hits carriage return/enter
+        if (Input.GetKey(KeyCode.Return) || Input.GetKey(KeyCode.KeypadEnter))
         {
             GlobalController.INSTANCE.PlaySound(bleep);
             LevelLoader.INSTANCE.LoadLevel("_first_level");
         }
+    }
+
+    public void btnEnterClick()
+    {
+        if (playerName.text == "" || playerName.text == null)
+        {
+            txtPleaseEnterName.SetActive(true);
+            return;
+        }
+        else
+        {
+            txtPleaseEnterName.SetActive(false);
+        }
+
+        GlobalController.INSTANCE.CurrentPlayerName = playerName.text;
+        GlobalController.INSTANCE.PlaySound(bleep);
+        LevelLoader.INSTANCE.LoadLevel("_first_level");
     }
 }
